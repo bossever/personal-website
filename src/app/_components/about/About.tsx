6a1c1307ef6382fff786bbf02ac87b6d82a39a5e
Ayushman Sachan <ashusachan01@gmail.com>
2024-06-07 20:02:37 +0530

    About section responsive v1

--- src/app/_components/about/About.tsx
+++ src/app/_components/about/About.tsx
@@ -2,32 +2,57 @@ import Image from "next/image";
 import Section from "../shared/Section";
 import Typography from "../shared/Typography";
 import pic from "../../../../public/images/ayushman-picture.jpeg";
-import glyph from "../../../../public/images/headshot-glyph.svg";
+import glyphHeadshot from "../../../../public/images/headshot-glyph.svg";
+import glyphThick from "./../../../../public/images/glyph-thick.svg";
 
 export default function About() {
   return (
     <Section title="About me" id="about-me">
-      <div className="flex flex-col gap-6 tablet:flex-row-reverse">
-        <div className="p-10 relative tablet:p-0">
+      <div className="flex flex-col gap-6 tablet:flex-row-reverse laptop:gap-12 desktop:gap-44">
+        <div className="p-10 relative tablet:p-0 tablet:float-right laptop:float-none tablet:hidden laptop:block">
           <Image
-            className="absolute top-0 left-0 w-full -z-10 tablet:hidden"
-            src={glyph}
+            className="absolute top-0 left-0 w-full -z-10 tablet:hidden invert-0 dark:invert-[100]"
+            src={glyphHeadshot}
             alt=""
           />
           <Image
-            className="w-full"
             src={pic}
             alt="A picture of Ayushman Sachan, such a handsome lad!"
+            quality={100}
           />
         </div>
-        <Typography level="paragraph" extra={{
-          textAlign: 'justify'
-        }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean rutrum porttitor enim viverra dignissim.
-          Duis placerat massa non eleifend porttitor. Duis rhoncus tempor mollis. Quisque iaculis sodales vestibulum.
-          Vivamus at placerat massa, et blandit leo. Nunc pretium elementum risus, sit amet facilisis justo congue eu.
-          Quisque eu ligula ac quam pulvinar rutrum. Mauris id volutpat ipsum. Nullam efficitur lectus nec nunc blandit tincidunt.
-        </Typography>
+        <div>
+          <Image
+            className="hidden tablet:block tablet:float-right laptop:hidden px-4 pb-2"
+            width={300}
+            height={300}
+            src={pic}
+            alt="A picture of Ayushman Sachan, such a handsome lad!"
+            quality={100}
+          />
+          <Typography
+            level="paragraph"
+            extra={{
+              textAlign: "justify",
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            rutrum porttitor enim viverra dignissim. Duis placerat massa non
+            eleifend porttitor. Duis rhoncus tempor mollis. Quisque iaculis
+            sodales vestibulum. Vivamus at placerat massa, et blandit leo.
+          </Typography>
+          <br />
+          <Typography
+            level="paragraph"
+            extra={{
+              textAlign: "justify",
+            }}
+          >
+            Nunc pretium elementum risus, sit amet facilisis justo congue eu.
+            Quisque eu ligula ac quam pulvinar rutrum. Mauris id volutpat ipsum.
+            Nullam efficitur lectus nec nunc blandit tincidunt.
+          </Typography>
+        </div>
       </div>
     </Section>
   );

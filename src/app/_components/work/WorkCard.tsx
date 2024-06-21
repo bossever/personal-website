d5b21333b85849eb649df93aec4691827c226774
Ayushman Sachan <ashusachan01@gmail.com>
2024-06-21 20:56:11 +0530

    Add expand/collapse animation to work cards

--- src/app/_components/work/WorkCard.tsx
+++ src/app/_components/work/WorkCard.tsx
@@ -5,6 +5,7 @@ import Typography from "../shared/Typography";
 import WorkSubcard from "./WorkSubcard";
 import { useState } from "react";
 import { FaChevronDown, FaChevronRight } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 type WorkCardProps = {
   work: Work;
@@ -26,18 +27,32 @@ export default function WorkCard({ work, collapsed }: WorkCardProps) {
           onClick={toggleCollapse}
         >
           <div className="flex items-center gap-4">
-            {isCollapsed ? <FaChevronRight /> : <FaChevronDown />}
+            <motion.span
+              animate={{
+                rotateZ: isCollapsed ? 0 : 90,
+              }}
+            >
+              <FaChevronRight />
+            </motion.span>
             <Typography level="subheading">{work.company}</Typography>
           </div>
           <Typography level="subheading">{work.location}</Typography>
         </div>
-        {!isCollapsed && (
-          <div>
-            {work.roles.map((role, index) => (
-              <WorkSubcard key={index} role={role} />
-            ))}
-          </div>
-        )}
+        <motion.div
+          className="overflow-hidden"
+          initial={{
+            height: 0,
+            opacity: 0,
+          }}
+          animate={{
+            height: isCollapsed ? 0 : "auto",
+            opacity: isCollapsed ? 0 : 1,
+          }}
+        >
+          {work.roles.map((role, index) => (
+            <WorkSubcard key={index} role={role} />
+          ))}
+        </motion.div>
       </div>
     </>
   );
